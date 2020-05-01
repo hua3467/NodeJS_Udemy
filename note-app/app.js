55e6381767f2e3db4a9df43c5122295aed396a20
Aaron Yang <zhenhuayang@outlook.com>
2020-05-01 15:23:07 -0500

    note app: yargs installed

--- note-app/app.js
+++ note-app/app.js
@@ -1,7 +1,37 @@
-const validator = require('validator');
-const notes = require('./notes');
+const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-const msg = notes();
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note.',
+    handler: function(){
+        console.log('Add a new note.')
+    }
+})
 
-console.log(msg);
-console.log(validator.isEmail('Aaron@email.com'));
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note.',
+    handler: function(){
+        console.log('removing the note')
+    }
+})
+
+yargs.command({
+    command: 'list',
+    describe: 'List all the notes',
+    handler: function(){
+        console.log('listing the notes')
+    }
+})
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a note',
+    handler: function(){
+        console.log('reading the note...');
+    }
+})
+
+console.log(yargs.argv);
