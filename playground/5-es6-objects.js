5a38bbd6ac56909a26f84f8df74b88872430df16
Aaron Yang <zhenhuayang@outlook.com>
2020-07-21 23:22:37 -0500

    deconstruction shorthand

---
+++ playground/5-es6-objects.js
@@ -0,0 +1,36 @@
+// Object property shorthand
+
+const name = "Andrew";
+const userAge = 27;
+
+const user = {
+    name: name,
+    age: userAge,
+    location: "New York"
+}
+
+console.log(user);
+
+// Object destructring
+
+const product = {
+    label: "Red notebook",
+    price: 3,
+    stock: 201,
+    salePrice: undefined
+}
+
+// const {label, stock} = product
+// console.log(label);
+// console.log(stock);
+
+// const {label: productLable, stock, rating = 5} = product;
+// console.log(productLable);
+// console.log(stock);
+// console.log(rating);
+
+const transaction = (type, { label, stock}) => {
+    console.log(type, label, stock);
+}
+
+transaction("order", product);
