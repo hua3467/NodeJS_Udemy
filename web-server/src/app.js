cdd246fa4396decb2829be3def0a1f883068ae71
Aaron Yang <zhenhuayang@outlook.com>
2020-07-24 20:50:38 -0500

    handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,47 @@
+const path = require("path");
 const express = require("express");
+const hbs = require("hbs");
 
 const app = express();
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../tamplates/partials");
+
+// Setup handlebars engin and views location
+app.set('view engine', 'hbs');
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
 app.get("", (req, res) => {
-    res.send("Hello express!");
+    res.render("index", {
+        title: "Weather App",
+        name: "Aaron Yang"
+    });
+});
+
+app.get("/about", (req, res) => {
+    res.render("about", {
+        title: "About the App",
+        name: "Aaron Yang"
+    })
+})
+
+app.get("/help", (req, res) => {
+    res.render("help", {
+        title: "Help",
+        name: "Aaron Yang"
+    })
+})
+
+app.get("/weather", (req, res) => {
+    res.send("Weather");
 });
 
 app.listen(3000, () => {
-    console.log("Serveri is up on port 3000.");
+    console.log("Server is up on port 3000.");
 });
