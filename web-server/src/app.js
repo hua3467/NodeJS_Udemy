e9efef7651c007df5cec0f591e09c8b4ccdffb9d
Aaron Yang <zhenhuayang@outlook.com>
2020-07-31 22:25:22 -0500

    default parameter value

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
 
 const app = express();
 
@@ -39,17 +41,59 @@ app.get("/help", (req, res) => {
 })
 
 app.get("/weather", (req, res) => {
-    res.send({
-        forecast: 'It is snowing',
-        location: 'Philadelphia'
-    });
+    if(!req.query.address){
+        return res.send({
+            error: "Please enter an address."
+        })
+    }
+
+    geocode(req.query.address, (error, { latitude, longtitude, location} = {}) => {
+
+        console.log(latitude, longtitude, location)
+        if (error) {
+            return res.send({ error})
+        }
+
+        forecast(latitude, longtitude, (error, forecastData) => {
+            if(error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location, 
+                address: req.query.address
+            })
+        })
+    })
+
+    // res.send({
+    //     forecast: 'It is snowing',
+    //     location: 'Philadelphia',
+    //     address: req.query.address
+    // });
 });
+
 
 app.get("/help/*", (req, res) => {
     res.render("404", {
         message: "Help article not fount."
     });
 });
+
+app.get("/products", (req, res) => {
+
+    if (!req.query.search) {
+        return res.send({
+            error: "You must provide a search term."
+        });
+    }
+
+    console.log(req.query);
+    res.send({
+        products: []
+    })
+})
 
 app.get("*", (req, res) => {
     res.render("404", {
